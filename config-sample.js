5c9b873f4afbdd378fcd993fbfde1fc1dab562b3
Braden Jonsson <braden337@gmail.com>
2016-05-01 23:09:52 -0500

    add sample config.js file

---
+++ config-sample.js
@@ -0,0 +1,10 @@
+module.exports = {
+  page_access_token: '<page-access-token>'
+}
+
+// Your <page-access-token> would be located at
+// https://developers.facebook.com/apps/<your-app-id>/messenger/
+// right at the top of the page
+//
+// After you put your page access token here, rename this file to
+// config.js
